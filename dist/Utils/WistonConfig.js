26b6c9020689b4772cbd8c6a37c28e1d4a4643fb
venkata-hari <xprogrammer.me@gmail.com>
2025-04-24 08:10:17 +0530

    some changes are made2

--- dist/Utils/WistonConfig.js
+++ dist/Utils/WistonConfig.js
@@ -4,16 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const winston_1 = __importDefault(require("winston"));
+const transports = [
+    new winston_1.default.transports.Console(), // only console log on Vercel
+];
+// Optional: only add file transport in local/dev
+if (process.env.NODE_ENV !== 'production') {
+    const fs = require('fs');
+    const logDir = 'logs';
+    if (!fs.existsSync(logDir)) {
+        fs.mkdirSync(logDir);
+    }
+    transports.push(new winston_1.default.transports.File({ filename: 'logs/error.log', level: 'error' }), new winston_1.default.transports.File({ filename: 'logs/combined.log' }));
+}
 const logger = winston_1.default.createLogger({
     level: 'info',
-    format: winston_1.default.format.combine(winston_1.default.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), winston_1.default.format.printf(({ timestamp, level, message }) => {
-        return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-    })),
-    transports: [
-        new winston_1.default.transports.Console(),
-        new winston_1.default.transports.File({ filename: 'logs/requests.log' }),
-        new winston_1.default.transports.File({ filename: 'logs/errors.log', level: 'error' })
-    ],
+    format: winston_1.default.format.combine(winston_1.default.format.timestamp(), winston_1.default.format.simple()),
+    transports,
 });
 exports.default = logger;
 //# sourceMappingURL=WistonConfig.js.map
